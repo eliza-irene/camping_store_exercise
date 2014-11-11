4944f605c77c54f0498e1a8e9a81c7b85b7c7895
eliza-irene <elizabeth.korthof@gmail.com>
2014-11-11 16:08:51 -0500

    Added animations to inventory table

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -30,3 +30,5 @@ angular
         redirectTo: '/'
       });
   });
+
+  
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -25,6 +25,7 @@ module.exports = function(config) {
       'bower_components/angular-messages/angular-messages.js',
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
+      'bower_components/underscore/underscore.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
       'test/spec/**/*.js'
